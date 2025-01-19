b1f8d8dbc6a1abd89afd22530e1f55e0a8763762
Bongani Shube <bonganishubeaz@gmail.com>
2025-01-19 08:06:46 +0200

    Fix deployment issues

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,17 +1,24 @@
 import ProductsContainer from '@/components/products/ProductsContainer';
 
-async function ProductsPage(
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+export default async function ProductsPage(
   props: {
-    searchParams: { layout?: string; search?: string };
+    searchParams: SearchParams;
   }
 ) {
-  const layout = props.searchParams.layout || 'grid';
-  const search = props.searchParams.search || '';
+  const resolvedSearchParams = await props.searchParams; // Resolve the promise
+  const layout = Array.isArray(resolvedSearchParams.layout)
+    ? resolvedSearchParams.layout.join(',') // Convert array to string
+    : resolvedSearchParams.layout || 'grid'; // Default to 'grid'
+
+  const search = Array.isArray(resolvedSearchParams.search)
+    ? resolvedSearchParams.search.join(',') // Convert array to string
+    : resolvedSearchParams.search || ''; // Default to an empty string
+
   return (
     <>
       <ProductsContainer layout={layout} search={search} />
     </>
   );
 }
-
-export default ProductsPage;
