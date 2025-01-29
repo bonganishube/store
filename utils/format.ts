9840dde6dbb16730ce9418fc5623a173b5dccd46
Bongani Shube <bonganishubeaz@gmail.com>
2025-01-29 09:54:58 +0200

    Change currency format to ZAR

--- utils/format.ts
+++ utils/format.ts
@@ -2,7 +2,7 @@ export const formatCurrency = (amount: number | null) => {
   const value = amount || 0;
   return new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "USD",
+    currency: "ZAR",
   }).format(value);
 };
 
